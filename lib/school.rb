55b9bd7071b7f3b5b660edb59e29e17dfa17ea38
mav-code <mavisansmembres@gmail.com>
2020-01-28 18:19:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @roster = Hash.new
+    end
+    def add_student(student, grade)
+        if @roster.key?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        output = Hash.new
+        @roster.each{|k,v| output[k] = v.sort}
+        return output
+    end
+end
